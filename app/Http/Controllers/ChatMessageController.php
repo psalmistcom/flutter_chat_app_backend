e316692793e33701a05349b80d4e4f5b78b0d0bf
psalmistcom <dadewale9@gmail.com>
2023-06-13 22:18:27 +0100

    store message request

--- app/Http/Controllers/ChatMessageController.php
+++ app/Http/Controllers/ChatMessageController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\GetMessageRequest;
+use App\Http\Requests\StoreMessageRequest;
+use App\Models\ChatMessage;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ChatMessageController extends Controller
 {
-    public function index(GetMessageRequest $request)
+    public function index(GetMessageRequest $request): JsonResponse
     {
+        $data = $request->validated();
+        $chatId = $data['chat_id'];
+        $currentPage = $data['page'];
+        $pageSize = $data['page_size'] ?? 15;
+
+        $messages = ChatMessage::where('chat_id', $chatId)
+            ->with('user')
+            ->latest('created_at')
+            ->simplePaginate(
+                $pageSize,
+                ['*'],
+                'page',
+                $currentPage
+            );
+
+        return $this->success($messages->getCollection());
     }
 
 
-    public function store()
+    public function store(StoreMessageRequest $request)
     {
     }
 }
