bbc34a1ec5a0e9595866bbdbdff222b1f32a60ca
psalmistcom <dadewale9@gmail.com>
2023-06-13 21:43:32 +0100

    chat message controller and message requests

---
+++ app/Http/Controllers/ChatMessageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GetMessageRequest;
+use Illuminate\Http\Request;
+
+class ChatMessageController extends Controller
+{
+    public function index(GetMessageRequest $request)
+    {
+    }
+
+
+    public function store()
+    {
+    }
+}
