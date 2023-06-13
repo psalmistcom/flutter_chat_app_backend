d61239848905ddae6853f0e54f64742a3d7eae08
psalmistcom <dadewale9@gmail.com>
2023-06-13 18:30:56 +0100

    auth endpoints done

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function success(mixed $data, string $message = "okay", int $statusCode = 200): JsonResponse
+    {
+        return response()->json([
+            'data' => $data,
+            'success' => true,
+            'message' => $message,
+        ], $statusCode);
+    }
+
+    public function error(string $message, int $statusCode = 400): JsonResponse
+    {
+        return response()->json([
+            'data' => null,
+            'success' => false,
+            'message' => $message,
+        ], $statusCode);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('auth')
+    ->as('auth.')
+    ->group(function () {
+
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('register', [AuthController::class, 'register'])->name('register');
+        Route::post('login_with_token', [AuthController::class, 'loginWithToken'])
+            ->middleware('auth:sanctum')
+            ->name('login_with_token');
+        Route::get('logout', [AuthController::class, 'logout'])
+            ->middleware('auth:sanctum')
+            ->name('logout');
+    });
